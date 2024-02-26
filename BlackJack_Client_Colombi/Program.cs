a2cadfb5a310664e5b45d263de4da13dc2422cca
ColombiAlessandro <colombi.alessandro.studente@itispaleocapa.it>
2024-02-26 01:56:06 +0100

    prova1

--- BlackJack_Client_Colombi/Program.cs
+++ BlackJack_Client_Colombi/Program.cs
@@ -1,15 +1,81 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace BlackJack_Client_Colombi
 {
-    internal class Program
+    class Program
     {
+
         static void Main(string[] args)
         {
+            
+            TcpClient cliente = new TcpClient();
+            NetworkStream stream;
+            Boolean ok = true;
+            int porta = 9000;
+            string nome_server = "127.0.0.1";
+            try
+            {
+                Console.Write("Vuoi iniziare la partita? [Y/N]");
+                string start = Console.ReadLine();
+                if (start == "Y" || start == "y")
+                {
+                    // tenta di connettersi al server specificato
+                    cliente.Connect(nome_server, porta);
+                    // Delega la gestione del flusso dati all'oggetto networkstream, in modo
+                    // identico a quanto già visto per il Server.
+                    stream = cliente.GetStream();
+                    // Verifica che il flusso sia di lettura e scrittura
+                    if (!stream.CanRead)
+                    {
+                        Console.WriteLine("Non posso leggere dallo Stream");
+                        ok = false;
+                    }
+                    else
+                        if (!stream.CanWrite)
+                    {
+                        Console.WriteLine("Non posso scrivere nello Stream");
+                        ok = false;
+                    }
+                    if (ok)
+                    {
+                        Byte[] bytes = new Byte[cliente.ReceiveBufferSize];
+
+                        int numero_bytes; //numero di byte letti o scritti
+                        String stringa_inviata;
+                        String stringa_ricevuta;
+                        Console.WriteLine("Connesso al Server!");
+                        // I flussi in uscita e in ingresso sono gestiti esattamente come
+                        // già visto nell'applicazione Server...
+                        do
+                        {
+                            Byte[] bytes_da_inviare = Encoding.ASCII.GetBytes("S" + "\n");
+                            stream.Write(bytes_da_inviare, 0, bytes_da_inviare.Length);
+                            numero_bytes = stream.Read(bytes, 0, cliente.ReceiveBufferSize);
+                            stringa_ricevuta = Encoding.ASCII.GetString(bytes, 0, numero_bytes);
+                            Console.WriteLine("Ricevuto: " + stringa_ricevuta);
+                            if (stringa_ricevuta == "W")
+                            {
+                                Thread.Sleep(1000);
+                            }
+                        } while (stringa_ricevuta != "S");
+                        Console.WriteLine("Gioco iniziato");
+                        Console.ReadLine();
+                    }
+                    cliente.Close();
+                }
+            }
+            catch (Exception eccezione)
+            {
+                Console.WriteLine(eccezione.ToString());
+            }
+
         }
     }
 }
--- BlackJack_Server_Colombi/Program.cs
+++ BlackJack_Server_Colombi/Program.cs
@@ -1,15 +1,117 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
+using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace BlackJack_Server_Colombi
 {
-    internal class Program
+    
+    class Program
     {
+        static int porta = 9000;
+        public static int counter;
         static void Main(string[] args)
         {
+            TcpListener serverSocket = new TcpListener(IPAddress.Any, porta);
+            TcpClient clientSocket = default(TcpClient);
+
+            serverSocket.Start();
+            Console.WriteLine("Server avviato - Porta: " + porta);
+
+            counter = 0;
+            while (true)
+            {
+                counter += 1;
+                clientSocket = serverSocket.AcceptTcpClient();
+                Console.WriteLine("Client Numero:" + Convert.ToString(counter) + " avviato!");
+                var port = ((IPEndPoint)clientSocket.Client.RemoteEndPoint).Port;
+                Console.WriteLine("Il client lavora sulla porta " + port);
+                handleClient client = new handleClient();
+                client.startClient(clientSocket, Convert.ToString(counter));
+            }
+
+            serverSocket.Stop();
+            Console.WriteLine("Esco (in realtà non avviene mai, per come è scritto questo codice)");
+            Console.ReadLine();
+        }
+    }
+
+    //Classe che gestisce ogni client connesso separatamente
+    public class handleClient
+    {
+        TcpClient clientSocket;
+        string clNo;
+        public void startClient(TcpClient inClientSocket, string clineNo)
+        {
+            this.clientSocket = inClientSocket;
+
+            this.clNo = clineNo;
+            Thread ctThread = new Thread(doChat);
+            ctThread.Start();
+        }
+        private void doChat()
+        {
+            int requestCount = 0;
+            byte[] bytesFrom = new byte[clientSocket.ReceiveBufferSize];
+            Console.WriteLine("La connessione è attiva all'indirizzo/porta: " + clientSocket.Client.RemoteEndPoint.ToString());
+            int num_bytesFrom; //numero di byte letti o scritti
+            string dataFromClient = null;
+            Byte[] sendBytes = null;
+            string serverResponse = null;
+            requestCount = 0;
+            bool connesso = true;
+
+            while (connesso)
+            {
+                try
+                {
+                    requestCount = requestCount + 1;
+                    NetworkStream networkStream = clientSocket.GetStream();
+                    // Legge fino al terminatore "\n"
+                    dataFromClient = "";
+                    do
+                    {
+                        num_bytesFrom = networkStream.Read(bytesFrom, 0, clientSocket.ReceiveBufferSize);
+                        dataFromClient += Encoding.ASCII.GetString(bytesFrom, 0, num_bytesFrom);
+                    } while (!dataFromClient.Contains("\n"));
+                    dataFromClient = dataFromClient.Replace("\n", "");
+                    Console.WriteLine(DateTime.Now + " - Ricevuto dal Client numero: " + clNo + " il messaggio: [" + dataFromClient + "]");
+
+                    if (!dataFromClient.Equals("QUIT"))
+                    {
+                        if (dataFromClient == "S")
+                        {
+                            if (Program.counter - 1 < 2)
+                            {
+                                serverResponse = "W";
+                            } else
+                            {
+                                serverResponse = "S";
+                            }
+                        } else serverResponse = "[ok]";
+                        sendBytes = Encoding.ASCII.GetBytes(serverResponse);
+                        networkStream.Write(sendBytes, 0, sendBytes.Length);
+                        networkStream.Flush();
+                        Console.WriteLine(DateTime.Now + " - Invio messaggio di conferma ricezione al Client numero: " + clNo);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Il Client numero: " + clNo + " si è disconnesso");
+                        connesso = false;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Errore: " + ex.Message);
+                    Console.WriteLine("Il Client numero: " + clNo + " si è disconnesso");
+                    connesso = false;
+                }
+            }
         }
     }
 }
